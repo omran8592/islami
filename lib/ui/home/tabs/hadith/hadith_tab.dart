dd1548f51fece9d4526869c99a95ab2c1426082d
Abdulrahman <abdoomran618@gmail.com>
2025-04-29 05:49:00 +0300

    + Added carousel_slider + Added Hadith files

--- lib/ui/home/tabs/hadith/hadith_tab.dart
+++ lib/ui/home/tabs/hadith/hadith_tab.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HadithTab extends StatelessWidget {
@@ -5,6 +6,22 @@ class HadithTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var height = MediaQuery.of(context).size.height;
+    return CarouselSlider(
+      options: CarouselOptions(height: 400.0),
+      items:
+          [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(color: Colors.amber),
+                  child: Text('text $i', style: TextStyle(fontSize: 16.0)),
+                );
+              },
+            );
+          }).toList(),
+    );
   }
 }
